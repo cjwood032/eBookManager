7bd674c5014c43c583ee09298b2f91a0c07f13f2
cjwood032 <38894141+cjwood032@users.noreply.github.com>
2020-10-14 14:25:28 -0400

    Chapter 2 Completed

---
+++ eBookManager.Engine/StorageSpace.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eBookManager.Engine
+{
+    public class StorageSpace
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Document> BookList { get; set; }
+    }
+}
